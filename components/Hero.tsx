27dc4a63d7bb992896a08cda3586992c1f3e4ca0
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-06-27 20:27:29 +0545

    update global styles, implement Quicksand font, and create Hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section>
+      <div className='flex flex-col p-2 w-fit'>
+        {/* Header and subheader */}
+        <h1 className='lowercase text-7xl font-bold text-soft'>
+          Sabin Shrestha.
+        </h1>
+        <h3 className='text-right text-muted'>I don&apos;t just <span className='italic text-heading'>build APIs</span>, I  <span className='italic text-heading'>build foundations.</span></h3>
+      </div>
+
+      {/* Let's build a console like simulation here. */}
+
+    </section>
+  )
+}
+
+export default Hero
