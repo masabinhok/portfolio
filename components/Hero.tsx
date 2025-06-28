12e3ea73f73bb4e0e3cc8ffb827868a33a3fc5b5
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-06-28 12:09:36 +0545

    fix: adjust layout of Hero component for better responsiveness

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section>
       <div className='flex gap-10'>
-        <div className='flex flex-col p-2 w-fit mb-10 w-full'>
+        <div className='flex flex-col p-2 mb-10 w-full'>
           {/* Header and subheader */}
           <h1 className='lowercase text-7xl font-bold text-soft'>
             Sabin Shrestha.
