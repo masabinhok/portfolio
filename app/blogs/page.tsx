34279ca5f606d1fe813ba6350d3b52d476a91a21
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-07-08 17:21:36 +0545

    add new pages

---
+++ app/blogs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Blogs = () => {
+  return (
+    <main className='p-5 min-h-screen flex items-center justify-center'>
+      Blogs
+    </main>
+  )
+}
+
+export default Blogs
---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <main className='p-5 min-h-screen flex items-center justify-center'>
+      Projects
+    </main>
+  )
+}
+
+export default Projects
